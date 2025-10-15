eb00b150a7d10d065c1da8f7dc0241666c8fc000
BartlomiejWos <bartlomiej.wos01@gmail.com>
2025-10-16 01:11:18 +0200

    add iq parser and recorded data

---
+++ postproc/EDA.m
@@ -0,0 +1,13 @@
+% Parameters based on mmWave sensor configuration 
+
+filename = "sfcw_512_Raw_0.bin";  % The name of the file containing raw data
+numADCSamples = 256;    % number of ADC samples per chirp 
+numADCBits = 16;        % number of ADC bits per sample 
+numRX = 4;              % number of receivers 
+isReal = false;         % set to "true" if real only data, "false" if complex data
+
+
+
+[retval, numChirps] = rawDataParser(filename, numADCSamples, numADCBits, numRX, isReal);
+size(retval)
+disp(numChirps)
